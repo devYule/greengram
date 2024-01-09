f15101e5d137d56ab261ae0455cbc1c8a17c4985
jo <yuyule20@naver.com>
2024-01-09 12:30:12 +0900

    2024-01-09

---
+++ src/main/java/com/green/greengram4/security/MyPrincipal.java
@@ -0,0 +1,15 @@
+package com.green.greengram4.security;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class MyPrincipal {
+
+    private int iuser;
+
+
+}
